813632492937c90862980e77e9af3e08c9c5c997
Shubham Kumar <developer.shubham07@gmail.com>
2022-08-13 13:40:45 +0530

    adding functionality to move surrounding with speed variable

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include "raymath.h"
+
 
 int main()
 {
@@ -6,8 +8,21 @@ int main()
     const int windowHeight{384};
     InitWindow(windowWidth, windowHeight, "Classy Class RPG");
 
+
+
+
     // Imporing Map to the Raylib window
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+
+
+
+
+    // Demifing vector and speed for assinging position to map
+    Vector2 mapPos{0.0, 0.0};
+    float speed{4.0};
+
+    
+
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -18,8 +33,27 @@ int main()
 
 
 
+        // creating if else statments to take key inputs
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+
+        if (IsKeyDown(KEY_LEFT)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_RIGHT)) direction.x += 1.0;
+        if (IsKeyDown(KEY_UP)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_DOWN)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
+            // set mapPos = mapPos - direction
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
+
+
+
         // Drawing Map Texture to Raylib window in each frame
-        Vector2 mapPos{0.0, 0.0};
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
 
