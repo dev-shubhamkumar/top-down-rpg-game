d0327132fb32c2eeac49ccab52e720875a758651
Shubham Kumar <developer.shubham07@gmail.com>
2022-08-14 20:54:14 +0530

    adding Classes to simplify the code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,76 @@
 #include "raymath.h"
 
 
+class Character
+{
+    public:
+        Vector2 getWorldPos() {return worldPos;}
+        void setScreenPos(int winWidth, int winHeight);
+        void tick(float deltaTime);
+    private:
+        Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+        Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+        Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+        Vector2 screenPos;
+        Vector2 worldPos;
+        // 1 : facing right direction : -1 : facing left direction
+        float rightLeft{1.f};
+        // animation variables
+        float runningTime{};
+        int frame{};
+        const int maxFrames{6};
+        const float updateTime{1.f / 12.f};
+        const float speed{4.f};
+};
+
+void Character::setScreenPos(int winWidth, int winHeight)
+{
+    screenPos = {
+        (float)winWidth/2.0f - 4.0f * (0.5f * (float)texture.width/6.0f),
+        (float)winHeight/2.0f - 4.0f * (0.5f * (float)texture.height)
+    };
+}
+
+void Character::tick(float deltaTime)
+{
+    // creating if else statments to take key inputs to move the map
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+    if (IsKeyDown(KEY_D)) direction.x += 1.0;
+    if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+    if (IsKeyDown(KEY_S)) direction.y += 1.0;
+
+    if (IsKeyDown(KEY_LEFT)) direction.x -= 1.0;
+    if (IsKeyDown(KEY_RIGHT)) direction.x += 1.0;
+    if (IsKeyDown(KEY_UP)) direction.y -= 1.0;
+    if (IsKeyDown(KEY_DOWN)) direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0)
+    {
+        // set worldPos = worldPos + direction
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;     // using terniary operator
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+
+
+    // upadate animation frame
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames) frame = 0;
+    }
+}
+
+
+
+
+
 int main()
 {
     const int windowWidth{384};
@@ -34,7 +104,7 @@ int main()
     float runningTime{};
     int frame{};
     const int maxFrames{6};
-    const float updateTime(1.f / 12.f);
+    const float updateTime{1.f / 12.f};
 
     
 
