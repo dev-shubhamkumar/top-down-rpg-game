f90f2b73fa7fa5f25ade1a8b314f72043883b648
Shubham Kumar <developer.shubham07@gmail.com>
2022-08-15 16:32:59 +0530

    adding pointers of Character in Enemy Class

--- main.cpp
+++ main.cpp
@@ -45,6 +45,7 @@ int main()
         LoadTexture("characters/goblin_idle_spritesheet.png"),
         LoadTexture("characters/goblin_run_spritesheet.png")
     };
+    goblin.setTarget(&knight);
 
 
 
